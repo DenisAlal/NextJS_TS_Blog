7e042244e26da54cc3a2bf366181120df8a8524e
DenisAlal <denisalalykin2001@gmail.com>
2023-09-05 15:46:19 +0700

    startPage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,10 @@
 import React, {useContext, useEffect, useState} from 'react';
-import { Button, Htag, P, Rating, Tag } from '@/components';
 import { withLayout } from '@/layout/Layout';
 import { AppContext } from "@/context/app.context";
-import axios from "axios";
-import {BlogData} from "@/interfaces/blogData.interface";
-import parse from "html-react-parser";
 import {LoadContext} from "@/context/load.context";
 function Home(): JSX.Element {
-	const [rating, setRating] = useState<number>(4);
 	const [userDataIndex, setUserDataIndex] = useState('');
 	const { isAuthenticated, setIsAuthenticated, userData } = useContext(AppContext);
-
-	const {isLoading, setIsLoading} = useContext(LoadContext);
-	const handleLogin = () => {
-		setIsAuthenticated(true);
-	};
-
-	const handleLogout = () => {
-		setIsAuthenticated(false);
-	};
 	useEffect(() =>{
 		console.log(isAuthenticated);
 	},[isAuthenticated]);
@@ -30,7 +16,7 @@ function Home(): JSX.Element {
 
 	return (
 		<>
-
+			Стартовая страница данного блога.
 		</>
 	);
 }
